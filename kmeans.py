31d8ff3ffe8b962f5733a63a4d347999091c4c21
vsgirelli <vavi_sg@hotmail.com>
2021-04-27 21:23:00 -0300

    fixing identation

--- kmeans.py
+++ kmeans.py
@@ -1,8 +1,12 @@
 import utils as ut
 
 def k_means(data, k):
-  centroid = []
+    centroid = []
 
-  for i in range(k):
-      # for now I'll just randomly create the first centroids based on the instances
-      centroid.append(ut.random_centroid(data))
+    for i in range(k):
+        # for now I'll just randomly create the first centroids based on the instances
+        centroid.append(ut.random_centroid(data))
+
+
+
+
--- utils.py
+++ utils.py
@@ -2,16 +2,16 @@ import math
 import random
 
 def manhattan_distance(p1, p2):
-  total = 0
-  for i in range(len(p1)):
-    total += abs(p1[i] - p2[i])
-  return total
+    total = 0
+    for i in range(len(p1)):
+        total += abs(p1[i] - p2[i])
+    return total
 
 def euclidean_distance(p1, p2):
-  total = 0
-  for i in range(len(p1)):
-    total += pow(p1[i] - p2[i])
-  return math.sqrt(total)
+    total = 0
+    for i in range(len(p1)):
+        total += pow(p1[i] - p2[i])
+    return math.sqrt(total)
 
 def get_max_min(axis):
     # get max and min from an axis
